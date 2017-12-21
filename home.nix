144db40e1a9ab14b773d476f090c22e1bda08fb0
Mogria <m0gr14@gmail.com>
2017-12-21 20:51:47 +0100

    configured git and, add some basic programs

--- home.nix
+++ home.nix
@@ -1,4 +1,17 @@
+{ pkgs, ... }:
+
 {
+  home.packages = [
+    pkgs.jq
+    pkgs.tree
+  ];
+
   programs.home-manager.enable = true;
   programs.home-manager.path = "https://github.com/rycee/home-manager/archive/master.tar.gz";
+
+  programs.git = {
+    enable = true;
+    userName = "Mogria";
+    userEmail = "m0gr14@gmail.com";
+  };
 }
