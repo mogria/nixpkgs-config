8db288e71ae5b80015ca4084b208890e83c3eb58
Mogria <m0gr14@gmail.com>
2018-10-04 18:42:48 +0200

    reconfigure fzf

--- home.nix
+++ home.nix
@@ -193,12 +193,12 @@ in {
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
-    defaultCommand = [ "${pkgs.fd}/bin/fd" "--type" "f" ];
-    defaultOptions = [ "--height 40%" "--border" ];
-    fileWidgetCommand = [ "${pkgs.fd}/bin/fd" "--type" "f" ];
-    fileWidgetOptions = [ "--preview 'file {}; head {}'" ];
-    changeDirWidgetCommand = [ "${pkgs.fd}/bin/fd" "--type" "d" ];
-    changeDirWidgetOptions = [ "--preview 'tree -C {} | head -200'" ];
+    defaultCommand = "${pkgs.fd}/bin/fd --type f";
+    defaultOptions = [ "--height" "40%" "--border" ];
+    fileWidgetCommand = "${pkgs.fd}/bin/fd --type f";
+    fileWidgetOptions = [ "--preview" "'file {}; head {}'" ];
+    changeDirWidgetCommand = "${pkgs.fd}/bin/fd --type d";
+    changeDirWidgetOptions = [ "--preview" "'tree -C {} | head -200'" ];
     historyWidgetOptions = [ "--sort" "--exact" ];
   };
 
