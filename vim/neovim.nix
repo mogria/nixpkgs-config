220489fc12c563e2afdcb481f4fef66fa3dfc05c
Mogria <m0gr14@gmail.com>
2019-10-24 23:00:01 +0200

    vim: use neovim now

--- vim/neovim.nix
+++ vim/neovim.nix
@@ -1,170 +1,34 @@
 { pkgs, ...}:
-let
-  stdenv = pkgs.stdenv;
-in {
 
-  /*
-  programs.neovim = {
-    enable = true;
-    customRC = ''
-    '';
-  */
+{
 
-  home.packages = [
-    /* there is also still a vim in the nixos configuration :/ share it with root??? */
-
-    (pkgs.vim_configurable.override {
-      pythonSupport = true; # needed for YouCompleteMe Plugin
-      python = pkgs.python3; # needed for YouCompleteMe Plugin
-      cscopeSupport = true; # gutentags plugin uses cscope
-    })
-  ];
 
   programs.zsh = {
-      shellAliases = {
-          "vi" = "vim";
-      };
       sessionVariables = {
         EDITOR = "vim";
         VISUAL = "vim";
       };
   };
 
-  home.file.".vimrc" = let
-    smallIndent = { size = 2; useTabs = false; };
-    fourIndent = { size = 4; useTabs = false; };
-    cssConf = {
-      indent = fourIndent;
-      addKeywordChars = "-";
+  home.packages = with pkgs; [
+    shellcheck
+  ];
+
+  programs.neovim = {
+    enable = true;
+    vimAlias = true;
+    viAlias = true;
+    withPython3 = true;
+    extraPython3Packages = (ps: with ps; [
+      python-language-server
+      pylint
+      sqlparse # for sqlformat, used as equalprg in vim
+    ]);
+    plugins = with pkgs.vimPlugins; [
+    ];
+    extraConfig = import ./config.nix {
+      inherit pkgs;
     };
-    shellConf = {
-      indent = fourIndent;
-      makeCommands = [{
-          filePattern = "*";
-          makeprg = "${pkgs.shellcheck}/bin/shellcheck\\ --check-sourced\\ --external-sources\\ --format=gcc";
-      }];
-    };
-    languages = {
-      "nix" = {
-        indent = smallIndent;
-        makeCommands = [
-          { filePattern = "*/.config/nixpkgs/*"; makeprg = "home-manager\\ switch;echo"; }
-          { filePattern = "/etc/nixos/*"; makeprg = "nixos-rebuild\\ switch;echo"; }
-          { filePattern = "*"; makeprg = "nix-repl"; }
-        ];
-        fileSearchPath = [ "/etc/nixos" "~/.config/nixpkgs" ];
-      };
-      "ruby" = {
-        indent = smallIndent;
-        makeCommands = [
-          { filePattern = "*"; makeprg = "rubocop"; }
-        ];
-      };
-      "python" = {
-        indent = fourIndent;
-        makeCommands = [
-          { filePattern = "*"; makeprg = "${pkgs.python37Packages.pylint}/bin/pylint"; }
-        ];
-      };
-      "yaml" = {
-        indent = smallIndent;
-      };
-      "css" = cssConf;
-      "scss" = cssConf;
-      "markdown" = {
-        indent = smallIndent;
-        addKeywordChars = "-";
-        makeCommands = [ { filePattern = "*"; makeprg = "~/bin/make-markdown"; } ];
-      };
-      "sh" = shellConf;
-      "bash" = shellConf;
-      "zsh" = shellConf;
-      "vim" = {
-        indent = smallIndent;
-        makeCommands = [
-          { filePattern = "*/.config/nixpkgs/vim/*"; makeprg = "home-manager\\ switch"; }
-        ];
-      };
-    };
-
-    getOrDefault = key: attrset: defaultValue:
-      if builtins.hasAttr key attrset
-        then builtins.getAttr key attrset
-        else defaultValue;
-
-    # general function which can generate
-    # vim config for an option for all languages
-    generateAllVimConfigLanguageOptions = option: vimConfigFunc:
-      let generateLanguageOptions = lang: languageOptions:
-        if builtins.hasAttr option languageOptions
-          then vimConfigFunc lang (builtins.getAttr option languageOptions)
-          else ""; # just generate an empty string if no option is set
-      in builtins.concatStringsSep "\n" (stdenv.lib.mapAttrsToList generateLanguageOptions languages);
-
-    indentVimConfig = let
-        vimConfigFunc = lang: indentOption: let
-          indent = toString (getOrDefault "size" indentOption 4); # default tab size
-          tabsOption = getOrDefault "useTabs" indentOption false; # don't use tabs per default
-          cIndent = if getOrDefault "cIndent" indentOption false # use smartindent as default
-              then "cindent"
-              else "smartindent";
-        in ''
-          autocmd FileType ${lang} set expandtab tabstop=${indent} softtabstop=${indent} shiftwidth=${indent} ${cIndent}
-          ${if tabsOption then "autocmd FileType ${lang} set noexpandtab" else ""
-          }'';
-      in generateAllVimConfigLanguageOptions "indent" vimConfigFunc;
-
-    makeCommandsVimConfig = let
-        vimConfigFunc = lang: makeCommandsOption: let
-          makeCommand = makeCommandOption: let
-            pattern = getOrDefault "filePattern" makeCommandOption "*";
-          in ''
-            if a:currentfile =~ glob2regpat('${pattern}')
-              setlocal makeprg=${makeCommandOption.makeprg}
-              return
-            endif
-          '';
-        in ''
-          function! Filetype_${lang}_set_makeprg(currentfile)
-          ${builtins.concatStringsSep "\n" (map makeCommand makeCommandsOption)}
-          endfunction
-          autocmd FileType ${lang} call Filetype_${lang}_set_makeprg(expand(expand("<afile>:p")))
-        '';
-      in generateAllVimConfigLanguageOptions "makeCommands" vimConfigFunc;
-
-    addKeywordCharsVimConfig = let
-        vimConfigFunc = lang: addKeywordCharsOption: ''autocmd FileType ${lang} set iskeyword+=${addKeywordCharsOption}'';
-      in generateAllVimConfigLanguageOptions "addKeywordChars" vimConfigFunc;
-
-    fileSearchPathVimConfig = let
-        vimConfigFunc = lang: fileSearchPathOption:
-          let path = builtins.concatStringsSep "," fileSearchPathOption;
-          in ''autocmd FileType ${lang} set path+=${path}'';
-      in generateAllVimConfigLanguageOptions "fileSearchPath" vimConfigFunc;
-
-
-
-  in {
-    text = ''
-      ${builtins.readFile ./vimrc};
-
-      " nix generated, language specific file search path configurations
-      ${fileSearchPathVimConfig}
-
-      " nix generated, language specific makeprg configurations
-      ${makeCommandsVimConfig}
-
-      " nix generated, language specific indentation configuration
-      ${indentVimConfig}
-
-      " nix generated, language specific keyword character definition
-      ${addKeywordCharsVimConfig}
-
-      let g:livepreview_previewer = '${pkgs.evince}'
-
-      " set equalprg for sql files to format sql
-      autocmd FileType sql setlocal equalprg=${pkgs.python37Packages.sqlparse}/bin/sqlformat\ --reindent\ --keywords\ upper\ --identifiers\ lower\ -
-    '';
   };
 
 }
