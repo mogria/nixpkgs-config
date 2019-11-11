805bdffba92cc8399558616a0ccbe83ccc45ce39
Mogria <m0gr14@gmail.com>
2019-11-11 17:55:48 +0100

    add bin/pbcopy for compatible script to open on Linux/MacOS

--- linux-packages.nix
+++ linux-packages.nix
@@ -12,6 +12,9 @@
     pkgs.gdb
     pkgs.gcc
     pkgs.cmake
+
+
+    pkgs.xsel # required by tmux for copy paste into X clipboard and for macos compatible pbcopy
   ]);
 
   systemd.user.startServices = lib.mkIf pkgs.stdenv.isLinux;
--- tmux/tmux.nix
+++ tmux/tmux.nix
@@ -4,7 +4,6 @@
   home.packages = [
     pkgs.tmux /* already installed by the nixos module */
     # pkgs.tmux-256colors-terminfo /* own terminfo for tmux */
-    pkgs.xsel # required by tmux for copy paste into X clipboard
   ];
 
   home.file.".tmux.conf" = {
@@ -76,9 +75,9 @@
       # use v or space to start selecting
       bind-key -T copy-mode-vi v send-keys -X begin-selection # 
       # use y or enter to copy the selection
-      bind-key -T copy-mode-vi Enter send-keys -X copy-pipe-and-cancel "xsel -i --clipboard"
+      bind-key -T copy-mode-vi Enter send-keys -X copy-pipe-and-cancel "pbcopy"
       # use y to yank a line into the clipboard
-      bind-key -T copy-mode-vi Y send-keys -X start-of-line \; send-keys -X start-of-line \; send-keys -X end-of-line \; send-keys -X copy-pipe-and-cancel "xsel -i --clipboard"
+      bind-key -T copy-mode-vi Y send-keys -X start-of-line \; send-keys -X start-of-line \; send-keys -X end-of-line \; send-keys -X copy-pipe-and-cancel "pbcopy"
       # copy selection into clipboard when releasing mouse
       bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-selection-and-cancel
       bind-key -T copy-mode-vi Escape send-keys -X cancel
