52ef0e9d770436df9e2763bf08e10bc822c4ad87
Mogria <m0gr14@gmail.com>
2019-11-11 22:52:27 +0100

    use horizon-dark colorscheme
    taken from: https://raw.githubusercontent.com/aaron-williamson/base16-alacritty/master/colors/base16-horizon-dark-256.yml

--- alacritty/alacritty.nix
+++ alacritty/alacritty.nix
@@ -4,5 +4,8 @@
   home.packages = [
     pkgs.alacritty
   ];
-  xdg.configFile."alacritty/alacritty.yml".text = builtins.readFile ./alacritty.yml;
+  xdg.configFile."alacritty/alacritty.yml".text = ''
+    ${builtins.readFile ./alacritty.yml}
+    ${builtins.readFile ./base16-horizon-dark-256.yml}
+  '';
 }
