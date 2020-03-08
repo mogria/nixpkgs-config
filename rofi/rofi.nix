e52d4d099c2751fa34b557bd951259fdf65b225d
Mogria <m0gr14@gmail.com>
2020-03-08 18:15:48 +0100

    rofi: use base16-horizon-dark theme

--- rofi/rofi.nix
+++ rofi/rofi.nix
@@ -14,27 +14,43 @@
     location = "top-right";
     xoffset = 0; # 150; # right beside the xfce panel
     yoffset = 0;
-    colors = let
-      red = "argb:90e01010";
-      white = "#a0a0a0";
-    in {
-      window = {
-        background = "argb:e0101010";
-        border = red;
-        separator = red;
-      };
+    # use base16-horizon-dark as the theme
+    extraConfig = ''
+      ! TAKEN FROM https://gitlab.com/0xdec/base16-rofi/-/blob/master/themes/base16-horizon-dark.config
 
-      rows = {
-        normal = {
-          foreground = white;
-          background = "argb:00000000";
-          backgroundAlt = "argb:90101010";
-          highlight = {
-            background = red;
-            foreground = white;
-          };
-        };
-      };
-    };
+      ! Base16 Horizon Dark
+      ! Author: Michaël Ball (http://github.com/michael-ball/)
+
+      ! base00: #1c1e26
+      ! base01: #232530
+      ! base02: #2e303e
+      ! base03: #6f6f70
+      ! base04: #9da0a2
+      ! base05: #cbced0
+      ! base06: #dcdfe4
+      ! base07: #e3e6ee
+      ! base08: #e93c58
+      ! base09: #e58d7d
+      ! base0A: #efb993
+      ! base0B: #efaf8e
+      ! base0C: #24a8b4
+      ! base0D: #df5273
+      ! base0E: #b072d1
+      ! base0F: #e4a382
+
+      ! Enable the extended coloring options
+      rofi.color-enabled: true
+
+      ! Property Name     BG       Border   Separator
+      rofi.color-window:  #232530, #232530, #1c1e26
+
+      ! Property Name     BG       FG       BG-alt   Head-BG  Head-FG
+      rofi.color-normal:  #232530, #cbced0, #232530, #232530, #e3e6ee
+      rofi.color-active:  #232530, #df5273, #232530, #232530, #df5273
+      rofi.color-urgent:  #232530, #e93c58, #232530, #232530, #e93c58
+
+      ! Set the desired separator style
+      rofi.separator-style: solid
+    '';
   };
 }
