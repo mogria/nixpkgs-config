a8f009a91dc96b0d3b33e9ae45fd1399e784f45b
Mogria <m0gr14@gmail.com>
2020-03-15 00:39:54 +0100

    4git: fix nix derivation to install 4git via home-manager

--- 4git/default.nix
+++ 4git/default.nix
@@ -1,23 +1,40 @@
-{ stdenv, bats }:
+{ stdenv, bats, git, coreutils, bash, fd }:
 
 stdenv.mkDerivation {
   pname = "4git";
-  version = "0.1.0";
+  version = "0.1.1";
   src = ./.;
 
-  nativeBuildInputs = [
-    bats
+  propagatedBuildInputs = [
+    git
+    bash
+    fd
   ];
 
-  # doCheck = true;
-  #
-  # checkPhase = ''
-  #   bats -r tests/
-  # '';
+  doCheck = true;
+
+  checkInputs = [
+    git
+    bats
+    coreutils
+  ];
+
+  buildPhase = ''
+    patchShebangs bin
+  '';
+
+  checkPhase = ''
+    # set git email and name, so no fatal errors
+    # are thrown constantly
+    export HOME=$TMPDIR
+    git config --global user.email "nobody@example.com"
+    git config --global user.name "Nobody"
+
+    bats -r tests/
+  '';
 
   installPhase = ''
-    mkdir -p $out/bin
-    cp $src/src/4git-* $out/bin
-    cp $src/src/*.sh $out/bin
+    mkdir -p $out
+    cp -r $src/bin $out/bin
   '';
 }
