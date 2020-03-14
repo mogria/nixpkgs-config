63e8e1c9cc0638862316901048365872e9fd4dc1
Mogria <m0gr14@gmail.com>
2020-03-14 17:03:43 +0100

    4git: fix test-helper after big rebase action

--- 4git/tests/test-helper.bash
+++ 4git/tests/test-helper.bash
@@ -5,7 +5,7 @@ set -u
 
 load git-repo
 
-FOURGIT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../src" && pwd)"
+FOURGIT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 TESTDATA_DIR="$(mktemp -d)"
 trap "test -n '$TESTDATA_DIR' && rm -rf '$TESTDATA_DIR'" EXIT
 export FOURGIT_DIR TESTDATA_DIR
@@ -15,11 +15,6 @@ export TMUX_WINDOW_NAME="FOURGIT_BATS_TESTSUITE"
 load tmux-mock
 load fzf-mock
 
-FOURGIT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-TESTDATA_DIR="$(mktemp -d)"
-trap "test -n '$TESTDATA_DIR' && rm -r '$TESTDATA_DIR'" EXIT
-export FOURGIT_DIR
-export TESTDATA_DIR
 export USE_TEST_MOCKS=1 # signal that 4gitlib.sh should include the *-mock.bash
                         # files in this directory for the mocked commands to be used
 
@@ -31,4 +26,4 @@ unset FOURGIT_WORKSPACE
 unset FOURGIT_BASE_BRANCH
 
 # make sure the scripts in the parent folder will be tested
-export PATH="$FOURGIT_DIR:$PATH"
+export PATH="$FOURGIT_DIR/src:$PATH"
