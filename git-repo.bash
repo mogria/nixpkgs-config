03728633904d591fcafbd1c286fb824158911e0a
Mogria <m0gr14@gmail.com>
2020-03-14 13:55:38 +0100

    4git: add git-repo.bash library to create temporary repos for testing
    I don't want to test these scripts from front to back manually. It's
    already tedious and I didn't write much code yet. So let's prepare
    everything for automated tests using bash.

---
+++ git-repo.bash
@@ -0,0 +1,45 @@
+
+setup_git_repo() {
+    REPOSITORY=`mktemp -d`
+    echo "$BATS_TEST_NUMBER" > "$REPOSITORY/4git-bats-test"
+    git init "$REPOSITORY"
+    cd "$REPOSITORY" || return 1
+
+    cat > "$REPOSITORY/README.md" <<README
+This is just a repository where some tests for 4git are being run.
+Feel free to delete this folder after the test run has finished.
+(It should get deleted automatically tough)
+README
+
+    mkdir "$REPOSITORY/src"
+    cat > "$REPOSITORY/src/main.c" <<MAIN_C
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char ** argv) {
+    printf("Hello, World");
+    return EXIT_SUCCESS;
+}
+MAIN_C
+
+    git add .
+    git commit -m 'Initial Commit'
+
+    export REPOSITORY
+}
+
+delete_git_repo() {
+    local repo="$REPOSITORY"
+    unset REPOSITORY
+
+    if [ -n "$KEEP_GIT_REPO" ]; then
+        echo "NOTE: Did not delete test git repository at $repo"
+        return 0
+    fi
+
+
+    if [ -n "$repo" ] && [ -f "$repo/4git-bats-test" ]  && [ "x$BATS_TEST_NUMBER" = "x$(cat "$repo/4git-bats-test")" ]; then
+        rm -r "$repo"
+    fi
+
+}
