5f413428adb3f807c35fc1df585b02394eda3ba4
Mogria <m0gr14@gmail.com>
2020-03-14 14:21:32 +0100

    4git: correctly handle the case where no workspace name is available

--- 4git/4gitinit.sh
+++ 4git/4gitinit.sh
@@ -6,6 +6,11 @@ set -u
 d="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$d/4gitlib.sh"
 
+error_no_workspace() {
+    echo "ERROR: This doesn't seem to be a 4git workspace, please create one first using 4git-workspace" 1>&2
+    exit 2
+}
+
 REAL_BASE_BRANCH="$(real_base_branch)"
 FOURGIT_WORKSPACE="$(4git_workspace)"
 
@@ -16,8 +21,7 @@ fi
 
 
 if [ -z "$FOURGIT_WORKSPACE" ]; then
-    echo "ERROR: this doesn't seem to be a 4git workspace, please create one first using 4git-create" 1>&2
-    exit 2
+    error_no_workspace
 fi
 
 
@@ -26,5 +30,9 @@ fi
 }
 
 FOURGIT_BASE_BRANCH="$(4git_branch)"
+
+if ! git rev-parse "$FOURGIT_BASE_BRANCH" 1> /dev/null; then
+    error_no_workspace
+fi
 
 export REAL_BASE_BRANCH FOURGIT_WORKSPACE FOURGIT_BASE_BRANCH
