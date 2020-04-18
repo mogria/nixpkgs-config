35278a1f2ea5dfd0398281fd8e12879609b7af58
Mogria <m0gr14@gmail.com>
2020-04-18 16:10:36 +0200

    vimpager: start up faster
    this trick from the github readme, set the filetype to itself with -c,
    then ansi esc doesn't even get loaded. Disabling it in the config file
    almost made no difference.

--- vim/vimpager.nix
+++ vim/vimpager.nix
@@ -1,10 +1,39 @@
 { pkgs, ...}:
 
-{
+let
+  # no asci esc here
+  vimpager-fast = { stdenv, lib, makeWrapper, vimpager-latest }:
+    stdenv.mkDerivation {
+      pname = "vimpager-fast";
+      version = "0.0.1";
+  
+      dontUnpack = true;
+      buildInputs = [ makeWrapper ];
+  
+      installPhase = ''
+        mkdir -p $out/bin
+        cp -aR ${vimpager-latest.out}/bin/* $out/bin
+
+        for f in $out/bin/*; do
+          [ ! -f "$f" ] && continue
+          [ ! -x "$f" ] && continue
+          wrapProgram "$f" \
+            --add-flags -c \
+            --add-flags "'set ft=&ft'"
+        done
+      '';
+
+      meta = with lib; {
+        description = "Vimpager which starts without ansiesc";
+        homepage = https://github.com/rkitover/vimpager;
+        license = licenses.mit;
+        maintainers = maintainers.mogria;
+        platforms = vimpager-latest.meta.platforms;
+      };
+    };
+in {
   home.packages = with pkgs; [
-    vimpager-latest
-    # there is also the nixpkgs.page package which uses
-    # neovim as a pager...
+    (callPackage vimpager-fast {})
   ];
 
   home.sessionVariables = {
