6786ea27b35ea89c68c4efeb8ed23497cc2d8fdc
Mogria <m0gr14@gmail.com>
2020-11-10 23:48:12 +0100

    vim: add coc-vim and lsp/autocomplete Support
    php/rust and some other languages have been tested as of now

--- vim/neovim.nix
+++ vim/neovim.nix
@@ -11,15 +11,53 @@ in {
   };
 
   home.packages = with pkgs; [
-    shellcheck
-    pythonPackages.sqlparse
+    neovim-remote # control nvim processes from terminal
+    proselint # lint text data
+
+    clang # ALE linter for C/C++ 
+    shellcheck # linter for shell scripts
+    shfmt # ale fixer for shell scripts
+    pythonPackages.sqlparse # ALE lint sql code (postgresql)
+    python37Packages.pylint # ALE lint python code # doesn't work because python enchant dependency has problems with python 2.7?
+    python37Packages.python-language-server # coc-python
     ripgrep # Used in vim-ripgrep plugin to provide :Rg command
+    plantuml # for building plantuml files
+    vim-vint # Ale Linter lint vimscript/vimrc
     racket # scheme dialect in use
+
+    nixpkgs-fmt # ale fixer (nixfmt exists as well)
+
+    bibclean # fix format BibTex bibliography files
+   # python-language-server
+
+    ## rust programming
+    # rls
+    # rustc
+    # cargo
+    rustup # toolchain installer, conflicts with others..
   ] ++ (with phpPackages; [
     php # ALE linter for php files
     phpcs # ALE fixer for php files
     composer # PHP-Package manager (essential for development and for ale to have vendor/bin/<executable>)
   ]);
+
+  xdg.configFile."nvim/coc-settings.json".text = ''
+  {
+    "suggest.enablePreview":  true,
+    "diagnostic.displayByAle": true,
+    "codeLens.enable": true,
+    "languageserver": {
+      "intelephense": {
+          "command": "intelephense",
+          "args": ["--stdio"],
+          "filetypes": ["php"],
+          "initializationOptions": {
+              "storagePath": "/tmp/intelephense"
+          }
+      }
+    },
+  }
+  '';
 
   programs.neovim = {
     enable = true;
@@ -27,11 +65,10 @@ in {
     viAlias = true;
     withPython3 = true;
     withPython = false;
-    withNodeJs = false;
+    withNodeJs = true;
     withRuby = false;
     extraPython3Packages = (ps: with ps; [
-      python-language-server
-      pylint
+      pynvim
     ]);
     plugins = with pkgs.vimPlugins; [
       # the best of the junegunn plugins
@@ -60,26 +97,62 @@ in {
       vim-airline
       vim-airline-themes
 
-      LanguageClient-neovim
+      # LanguageClient-neovim
+      coc-nvim
+      coc-vimtex
+      coc-python # achtung: benötigt :CocCommand python.setInterpreter pythonXX
+      coc-css
+      coc-html
+      coc-java
+      coc-yaml
+      coc-json
+      coc-neco
+      coc-fzf
+      coc-git
+      coc-yank
+      coc-rls # requires rustup package
 
       # install plugin to be able to install any base16 theme
       # but we just just horizon-dark
       base16-vim
 
-      syntastic
+      # asynchronous syntax checker and fixer
+      ale
 
       # language specific stuff
       plantuml-syntax
       vimtex # LaTeX
-      bats # tests for bash
+      sved
 
       # Snippets
       UltiSnips
+      coc-snippets
       vim-snippets
       neosnippet-snippets
 
       # Autocompletion
       # neocomplete  # currently doesn't work because this neovim apparently has no lua? Even though it seems to use luajit..
+      deoplete-nvim
+      neco-vim
+      deoplete-dictionary
+      deoplete-zsh
+      deoplete-clang
+      cpsm # fast fuzzy matching for completion
+      tmux-complete-vim # complete in vim from tmux panes
+      context_filetype-vim
+      coc-pairs
+      coc-neco
+      # nvim-lsp isused as the language server Language Server
+      # deoplete-lsp # nvim-lsp integration
+
+      # (pkgs.fetchUrl {
+      #   url = "https://github.com/thomasfaingnaert/vim-lsp-ultisnips/archive/494cd3c16b1d590e10c66d6cc10ace4dfe094085.zip";
+      #   sha256 = "1pl3gwzr5691jbm5f2fmicgqjzr4pvq7bl1fry7jbldr6ab74san";
+      # })
+      #
+
+      # display documentation
+      echodoc
 
       # easier commenting blocks (CTRL-/ CTRL-/ toggles line or selectino)
       tcomment_vim
@@ -90,6 +163,7 @@ in {
     extraConfig = import ./config.nix {
       inherit pkgs;
     };
+    package = pkgs.neovim-unwrapped;
   };
 
   /** install the few vim-plug plugins in a non-reproducable way
