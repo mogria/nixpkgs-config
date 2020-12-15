5af697ede9340267b4641e8192dabe1830ea689d
Mogria <m0gr14@gmail.com>
2020-12-15 13:19:17 +0100

    zsh: configure ssh-agent
    The oh-my-zsh plugin ssh-agent makes sure the ssh agent is running when
    using zsh.

--- zsh/zsh.nix
+++ zsh/zsh.nix
@@ -65,9 +65,12 @@
 
       oh-my-zsh= {
         enable = true;
-        plugins = [ "dircycle" "zsh-navigation-tools" ];
+        plugins = [ "dircycle" "zsh-navigation-tools" "ssh-agent" ];
         /* ugly hack: oh my zsh only wants a relative path, so lets go back to the system root */
-        theme = "../../../../../../../../../../../${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k";
+        theme = "../../../../../../../../../../../${pkgs.zsh-powerlevel9k}/share/zsh-powerlevel9k/powerlevel9k";
+        extraConfig = ''
+          zstyle :omz:plugins:ssh-agent identities id_ed25519
+        '';
       };
 
       sessionVariables = config.home.sessionVariables;
