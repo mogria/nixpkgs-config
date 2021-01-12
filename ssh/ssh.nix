3738bab8634602bc17d4874152d597d3891b3e46
Mogria <m0gr14@gmail.com>
2021-01-12 01:01:05 +0100

    ssh: fix matchBlocks configuration

--- ssh/ssh.nix
+++ ssh/ssh.nix
@@ -5,7 +5,18 @@
     enable = true;
     controlMaster  = "no";
     forwardAgent = false;
-    # hashKnownHosts = true;
+    hashKnownHosts = true;
+
+    matchBlocks = {
+      "github.com-guezzldynamic"= { # this domain is translated to github.com
+        identityFile = "~/.ssh/guezzldynamic_id_rsa"; # & this identityFile (private key is used);
+        hostname = "github.com";
+      };
+      "github.com" = {
+        identityFile = "~/.ssh/id_ecdsa";
+        hostname = "github.com";
+      };
+    };
   };
 
   home.packages = with pkgs; [
