5c6289412309241913bdd691b732ea5aff749362
Mogria <m0gr14@gmail.com>
2021-10-03 17:24:20 +0200

    rofi: fix color configuration

--- rofi/rofi.nix
+++ rofi/rofi.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 
-{
+
+let
+  background = "#232530";
+in {
   programs.rofi = {
     enable = true;
     font = "Monoid HalfTight Regular 16";
@@ -15,42 +18,63 @@
     xoffset = 0; # 150; # right beside the xfce panel
     yoffset = 0;
     # use base16-horizon-dark as the theme
-    extraConfig = ''
-      ! TAKEN FROM https://gitlab.com/0xdec/base16-rofi/-/blob/master/themes/base16-horizon-dark.config
+    #   ! TAKEN FROM https://gitlab.com/0xdec/base16-rofi/-/blob/master/themes/base16-horizon-dark.config
+    #
+    #   ! Base16 Horizon Dark
+    #   ! Author: Michaël Ball (http://github.com/michael-ball/)
+    #
+    #   ! base00: #1c1e26
+    #   ! base01: #232530
+    #   ! base02: #2e303e
+    #   ! base03: #6f6f70
+    #   ! base04: #9da0a2
+    #   ! base05: #cbced0
+    #   ! base06: #dcdfe4
+    #   ! base07: #e3e6ee
+    #   ! base08: #e93c58
+    #   ! base09: #e58d7d
+    #   ! base0A: #efb993
+    #   ! base0B: #efaf8e
+    #   ! base0C: #24a8b4
+    #   ! base0D: #df5273
+    #   ! base0E: #b072d1
+    #   ! base0F: #e4a382
+    colors = {
+      window = {
+        inherit background;
+        border = background;
+        separator = "#1c1e26";
+      };
 
-      ! Base16 Horizon Dark
-      ! Author: Michaël Ball (http://github.com/michael-ball/)
-
-      ! base00: #1c1e26
-      ! base01: #232530
-      ! base02: #2e303e
-      ! base03: #6f6f70
-      ! base04: #9da0a2
-      ! base05: #cbced0
-      ! base06: #dcdfe4
-      ! base07: #e3e6ee
-      ! base08: #e93c58
-      ! base09: #e58d7d
-      ! base0A: #efb993
-      ! base0B: #efaf8e
-      ! base0C: #24a8b4
-      ! base0D: #df5273
-      ! base0E: #b072d1
-      ! base0F: #e4a382
-
-      ! Enable the extended coloring options
-      rofi.color-enabled: true
-
-      ! Property Name     BG       Border   Separator
-      rofi.color-window:  #232530, #232530, #1c1e26
-
-      ! Property Name     BG       FG       BG-alt   Head-BG  Head-FG
-      rofi.color-normal:  #232530, #cbced0, #232530, #232530, #e3e6ee
-      rofi.color-active:  #232530, #df5273, #232530, #232530, #df5273
-      rofi.color-urgent:  #232530, #e93c58, #232530, #232530, #e93c58
-
-      ! Set the desired separator style
-      rofi.separator-style: solid
-    '';
+      rows = {
+        normal = {
+          inherit background;
+          foreground = "#cbced0";
+          backgroundAlt = background;
+          highlight = {
+            inherit background;
+            foreground = "#e3e6ee";
+          };
+        };
+        active = {
+          inherit background;
+          foreground = "#df5273";
+          backgroundAlt = background;
+          highlight = {
+            inherit background;
+            foreground = "#df5273";
+          };
+        };
+        urgent = {
+          inherit background;
+          foreground = "#e93c58";
+          backgroundAlt = background;
+          highlight = {
+            inherit background;
+            foreground = "#e93c58";
+          };
+        };
+      };
+    };
   };
 }
