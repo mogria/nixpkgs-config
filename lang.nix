0fff504b6123c5f22abebf3efd732816b1a0ecb4
Mogria <m0gr14@gmail.com>
2022-01-02 14:54:11 +0100

    lang: fix language environment variables

--- lang.nix
+++ lang.nix
@@ -2,12 +2,20 @@
 
 {
   home.language.base = "en_US.UTF-8";
+  home.language.address = "de_CH.UTF-8";
+  home.language.collate = "de_CH.UTF-8";
+  home.language.ctype = "de_CH.UTF-8";
+  home.language.measurement ="de_CH.UTF-8";
+  home.language.messages ="en_US.UTF-8";
   home.language.monetary ="de_CH.UTF-8";
+  home.language.name ="de_CH.UTF-8";
+  home.language.numeric ="de_CH.UTF-8";
   home.language.paper = "de_CH.UTF-8";
   home.language.time = "de_CH.UTF-8";
 
   home.sessionVariables = {
     LANGUAGE = "en_US.UTF-8";
     LANG = "en_US.UTF-8";
+    LC_ALL = "en_US.UTF-8";
   };
 }
