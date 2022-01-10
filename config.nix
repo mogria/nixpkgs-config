4d7bedbef8f219d0dac3657bd859f61aeebd9251
Mogria <m0gr14@gmail.com>
2022-01-10 03:18:38 +0100

    nur: update pinned version

--- config.nix
+++ config.nix
@@ -5,9 +5,9 @@
   packageOverrides = pkgs: {
     nur = import (builtins.fetchTarball {
       # Get the revision by choosing a version from https://github.com/nix-community/NUR/commits/master
-      url = "https://github.com/nix-community/NUR/archive/cfd5ba1c70c471bbee7981df94299cb7db3bb7c6.tar.gz";
+      url = "https://github.com/nix-community/NUR/archive/b353e4ce5723c24baf13b5c71d4b32901cd9ad7c.tar.gz";
       # Get the hash by running `nix-prefetch-url --unpack <url>` on the above url
-      sha256 = "0599yfdm3rm4hfx4jxxsxls9c5gfm38dgxmzdncqpw9irjkaqvbn";
+      sha256 = "1yhq3zn1wz96c344p372086i8q97z5hd0wb07p44qblw7j7s3093";
     }) {
       inherit pkgs;
     };
